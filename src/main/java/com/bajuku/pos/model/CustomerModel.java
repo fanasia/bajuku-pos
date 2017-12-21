53a21f56f02a1efb7f6b2cd88d69fca1182bb163
Fayya Nadhira Anyatasia <anyatasia@gmail.com>
2017-12-21 09:15:34 +0700

    create customer model

---
+++ src/main/java/com/bajuku/pos/model/CustomerModel.java
@@ -0,0 +1,49 @@
+package com.bajuku.pos.model;
+
+public class CustomerModel {
+    private int id;
+    private String email;
+    private String phone;
+    private String fullname;
+    private int points;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
