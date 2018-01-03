fdca0e168eb195d87e7794cc91c486c8dba20801
JOSHUA\Joshua_123 <joshuaivan.andjaya@gmail.com>
2018-01-03 21:56:03 +0800

    update product model

--- src/main/java/com/bajuku/pos/model/ProductModel.java
+++ src/main/java/com/bajuku/pos/model/ProductModel.java
@@ -4,9 +4,22 @@ public class ProductModel {
     private int id;
     private String fullname;
     private int category_id;
+    private String category_name;
     private float price;
     private int stock;
     private String mapping;
+
+    public ProductModel(){}
+
+    public ProductModel(int id, String fullname, int category_id, String category_name, float price, int stock, String mapping){
+        this.id= id;
+        this.fullname= fullname;
+        this.category_id= category_id;
+        this.category_name= category_name;
+        this.price= price;
+        this.stock= stock;
+        this.mapping= mapping;
+    }
 
     public int getId() {
         return id;
@@ -30,6 +43,14 @@ public class ProductModel {
 
     public void setCategory_id(int category_id) {
         this.category_id = category_id;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
     }
 
     public float getPrice() {
